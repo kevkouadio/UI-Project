57ffdde4acb2cdcbbf8a5571058de176e6f394b9
kevkouadio <kevkouadio@gmail.com>
2021-05-17 20:25:24 -0400

    first commit

---
+++ WebApplication1/Models/RssFeed.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class RssFeed
+    {
+        //public string Image { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ImageURL { get; set; }
+        public string DescriptionMarkup { get; set; }
+        public string Published { get; set; }
+    }
+
+   
+}
---
+++ WebApplication1/Models/RssReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Xml;
+namespace WebApplication1.Models
+{
+    public static class RssReader
+    {
+            public static IEnumerable<RssFeed> GetFeed()
+
+            {
+                string RSSURL = "https://www.theverge.com/rss/front-page/index.xml";
+
+            List<RssFeed> feeds = new List<RssFeed>();
+            XmlTextReader reader = new XmlTextReader(RSSURL);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+
+            if (feed != null)
+            {
+                feeds.AddRange(feed.Items.Select(i => new RssFeed
+                {
+                    Title = i.Title.Text,
+                    Published = i.PublishDate.DateTime.ToUniversalTime().ToString(CultureInfo.InvariantCulture),
+                    ImageURL = ((TextSyndicationContent)i.Content).Text,
+                    DescriptionMarkup = ((TextSyndicationContent)i.Content).Text,
+                    Author = i.Authors.FirstOrDefault().Name,
+                }));
+            }
+            return feeds;
+        }
+    }
+
+        
+}
